5603240d443e754e8de226b2a316f0014ef35c54
Henry Wu <hywu925@gmail.com>
2018-08-06 00:23:40 -0700

    Create Player class shell, get canvas to load

--- js/app.js
+++ js/app.js
@@ -24,11 +24,48 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+    constructor() {
+        this.x;
+        this.y;
+        this.sprite = 'images/char-boy.png';
+    }
+    
+    update(dt) {
+        return;
+    }
 
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput(direction) {
+        switch (direction) {
+            case 'left':
+                
+                break;
+            case 'up':
+
+                break;
+            case 'right':
+
+                break;
+            case 'down':
+
+                break;
+            default:
+                break;
+        }
+    }
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+let enemyOne = new Enemy();
+let player = new Player();
+let allEnemies = [enemyOne];
 
 
 
