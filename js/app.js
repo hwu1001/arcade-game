80fe6defebaefe532176827a83a6e1ee3cb5fd1b
Henry Wu <hywu925@gmail.com>
2018-08-07 23:39:17 -0700

    Handle player movement on board

--- js/app.js
+++ js/app.js
@@ -46,17 +46,26 @@ class Player {
 
     handleInput(direction) {
         switch (direction) {
+            // TODO: Define game constants for player movement limits
             case 'left':
-                
+                if (this.x > 0) {
+                    this.x -= 100;
+                }
                 break;
             case 'up':
-
+                if (this.y > -25) {
+                    this.y -= 85;
+                }
                 break;
             case 'right':
-
+                if (this.x < 400) {
+                    this.x += 100;
+                }
                 break;
             case 'down':
-
+                if (this. y < 400) {
+                    this.y += 85;
+                }
                 break;
             default:
                 break;
@@ -68,13 +77,14 @@ class Player {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-let enemyOne = new Enemy(0, 50, 1);
-let enemyTwo = new Enemy(0, 145, 1);
-let enemyThree = new Enemy(0, 225, 1);
-let player = new Player();
+let enemyOne = new Enemy(0, 60, 1);
+let enemyTwo = new Enemy(0, 140, 1);
+let enemyThree = new Enemy(0, 220, 1);
+// let player = new Player(200, 400, 1);
+let player = new Player(0, 400, 1);
 let allEnemies = [enemyOne, enemyTwo, enemyThree];
 
-
+// NOTE: Should move about 100 units is about distance for one square on the canvas
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -85,6 +95,7 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
-    player.handleInput(allowedKeys[e.keyCode]);
+    if (e.keyCode in allowedKeys) {
+        player.handleInput(allowedKeys[e.keyCode]);
+    }
 });
